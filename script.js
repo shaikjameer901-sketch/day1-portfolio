d5de6af9a164f4ba28d2e7a41e9e54508e61002f
shaikjameer901-sketch <shaikjameer901@gmail.com>
2026-05-23 09:44:23 +0530

    day 5: Added JavaScript + Ohm's Law Calculator

--- script.js
+++ script.js
@@ -7,6 +7,20 @@ function changeColor() {
     document.querySelector("h1").innerHTML = "Hi, I'm Jameer - ECE to Dev ⚡";
 }
 function calculateVoltage() {
+function calculateVoltage() {
+    let i = document.getElementById("current").value;
+    let r = document.getElementById("resistance").value;
+    
+    if(i === "" || r === "") {
+        document.getElementById("result").innerHTML = "Please enter both values ⚠️";
+        document.getElementById("result").style.color = "#ef4444";
+        return;
+    }
+    
+    let v = i * r;
+    document.getElementById("result").innerHTML = "V = " + v + " Volts ⚡";
+    document.getElementById("result").style.color = "#22c55e";
+}
     let i = document.getElementById("current").value;
     let r = document.getElementById("resistance").value;
     let v = i * r;
